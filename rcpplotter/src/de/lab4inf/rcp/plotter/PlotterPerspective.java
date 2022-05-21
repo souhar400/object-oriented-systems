5e3c03302a793cf7e46164057d6c61ab5b6915a3
lukas <lw874718@fh-muenster.de>
2022-05-21 12:24:14 +0200

    perspektive

---
+++ rcpplotter/src/de/lab4inf/rcp/plotter/PlotterPerspective.java
@@ -0,0 +1,15 @@
+package de.lab4inf.rcp.plotter;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PlotterPerspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.createFolder("left", IPageLayout.LEFT, 0.2f, IPageLayout.ID_EDITOR_AREA);
+		layout.createFolder("top", IPageLayout.TOP, 1.0f, IPageLayout.ID_EDITOR_AREA);
+
+	}
+
+}
